4c109147d4b5421f4640dcefdbf66f7c82fa2690
Dastanalymbekov <maxim.makarov.1997@mail.ru>
2017-09-21 15:26:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var x = 30;
+var y = 20;
+
+if(x < y) {
+console.log("Rabotaet if statement");
+}
+var x = 30;
+var y = 20;
+
+if (x < y) {
+    console.log("X menshe chem Y");
+} else {
+    console.log("X bolshy chem Y");
+}
+var x = 10;
+var y = 20;
+
+if (x < y) {
+    alert("X menshe chem Y");
+} else {
+    alert("X bolshy chem Y");
+}
+var years = prompt('how old are you?', 20);
+alert('you ' + years + ' years!')
